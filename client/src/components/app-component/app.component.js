0204bf741ac16404b3514f45dfbe7a4dacc52d17
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-04 18:05:29 -0600

    configurabled scss in react app

---
+++ client/src/components/app-component/app.component.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './app.component.scss';
+
+class AppComponent extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h2>Weather app</h2>
+      </div>
+    );
+  }
+}
+
+export default AppComponent;
