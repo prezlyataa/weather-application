0bc33fc2136c520dab99bf970071fd4e3b849415
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-07 11:07:52 -0600

    set appropriate icon in weather widget according to api

--- client/src/components/weather-widget-component/weather-widget.component.js
+++ client/src/components/weather-widget-component/weather-widget.component.js
@@ -9,17 +9,50 @@ const defaults = {
   animate: true
 };
 
+const ICONS = [
+  "CLEAR_DAY",
+  "CLEAR_NIGHT",
+  "PARTLY_CLOUDY_DAY",
+  "PARTLY_CLOUDY_NIGHT",
+  "CLOUDY",
+  "RAIN",
+  "SLEET",
+  "SNOW",
+  "WIND",
+  "FOG"
+];
+
 export class WeatherWidget extends Component {
   constructor(props) {
     super(props);
   }
+
+  formateStr = str => {
+    return str.toLowerCase().replace(/[`~!@#$%^&*()_|+\-=?;:'",.<>\{\}\[\]\\\/]/gi, '')
+  };
+
+  formanttedIcons = (icons) => {
+    return icons.map(icon => {
+      return this.formateStr(icon);
+    });
+  };
+
+  setIcon = icon => {
+    if(this.formanttedIcons(ICONS).some(i => {
+      return i === this.formateStr(icon);
+    })) {
+      return ICONS[this.formanttedIcons(ICONS).indexOf(this.formateStr(icon))];
+    } else {
+      return 'CLOUDY'
+    }
+  };
 
   render() {
     const { weatherData } = this.props;
     return (
       <div className="weather-widget">
         <ReactAnimatedWeather
-          icon={defaults.icon}
+          icon={this.setIcon(weatherData.currently.icon)}
           color={defaults.color}
           size={defaults.size}
           animate={defaults.animate}
