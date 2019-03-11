6443f0d4f9894b7093968b0a2514b2db9cde46a1
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-11 13:20:09 -0500

    moved '/' route to root

--- server/server.js
+++ server/server.js
@@ -18,5 +18,5 @@ app
   });
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
