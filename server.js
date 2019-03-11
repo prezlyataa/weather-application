560fcb759b5834cd296485265ccc956a70d027c7
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-11 11:19:41 -0500

    creadted procfile

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const weather = require("./weather/fetchWeather");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json(), cors());
@@ -15,20 +15,17 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });
 
-app.get("/weather", (req, res, next) => {
+app.get("/weather", (req, res) => {
   const address = res.req.query.address;
   const geocodeUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=
   ${address}&key=AIzaSyAbxqzC2IGT8BG6_OZV_UW_ANp5NTUTS6I`;
   weather.fetchWeather(geocodeUrl, res);
 });
 
-app.get("/test", (req,res,next) => {
+app.get("/test", (req,res) => {
   res.send({test: "test data"});
 });
 
-app.listen(port, '0.0.0.0', err => {
-  if (err) {
-    console.log(err);
-  }
+app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 });
