57cd53de5aadc002fbcaf9d971583dd8056e1300
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-11 12:59:08 -0500

    changed path in route

--- server/router.js
+++ server/router.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const weatherController = require("./controllers/weather-data.controller");
 
 router.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+  res.sendFile(path.join(__dirname + "../client/build/index.html"));
 });
 
 router.get("/weather", weatherController.getWeatherData);
