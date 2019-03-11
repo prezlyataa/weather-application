11567d197b66faa8d7a808cdf26887624ee745e9
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-11 12:34:29 -0500

    changed server structure

---
+++ server/router.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+const weatherController = require('./controllers/weather-data.controller');
+
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+router.get("/weather", weatherController.getWeatherData);
+
+router.get("/test", (req,res) => {
+  res.send({test: "test data"});
+});
+
+module.exports = router;
---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+const router = require("./router");
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+app
+  .use(express.static(path.join(__dirname, "client/build")))
+  .use(cors())
+  .use(bodyParser.json())
+  .use(bodyParser.urlencoded({ extended: true }))
+  .use("/", router)
+  .listen(port, () => {
+    console.log(`Listening on port ${port}`)
+  });
