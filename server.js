61337388c6f6403f9aea4b692d0fc3a06e71c9a4
Prezlyata Yuriy <Prezlyata Yuriy>
2019-03-11 10:54:40 -0500

    changed port

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const path = require("path");
 const weather = require("./weather/fetchWeather");
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.use(express.static(path.join(__dirname, "client/build")));
 app.use(bodyParser.json(), cors());
@@ -26,4 +26,9 @@ app.get("/test", (req,res,next) => {
   res.send({test: "test data"});
 });
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, '0.0.0.0', err => {
+  if (err) {
+    console.log(err);
+  }
+  console.log(`Listening on port ${port}`)
+});
